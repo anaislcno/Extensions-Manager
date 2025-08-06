2dffe348bc4b0702168b40997e04949a75d47c56
Anais <anais.lemire@mnhn.fr>
2025-08-06 20:05:47 +0200

    update config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/site.scss";
+import "./styles/site.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
