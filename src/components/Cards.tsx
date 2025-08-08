956bb18305d24f2ec38d61f18a0e137820e1e40a
Anais <anais.lemire@mnhn.fr>
2025-08-08 13:16:12 +0200

    update for accessibility

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -25,7 +25,7 @@ const Cards = ({ activeFilter }: CardsProps) => {
           className="container flex h-fit min-h-[194px] flex-col items-start justify-between gap-12 rounded-2xl border-2 border-neutral-200 bg-white p-16 shadow-[1px_1px_15px_0px_hsl(217,61%,90%)] md:max-w-[380px] dark:border-1 dark:border-neutral-600 dark:bg-neutral-800 dark:shadow-none"
         >
           <div className="flex items-start gap-12">
-            <img src={data.logo} alt="" />
+            <img src={data.logo} alt={`Logo ${data.name} extension`} />
             <div className="flex flex-col items-start gap-6">
               <h3 className="text-xl">{data.name}</h3>
               <p className="text-base font-normal">{data.description}</p>
@@ -39,11 +39,17 @@ const Cards = ({ activeFilter }: CardsProps) => {
             >
               Remove
             </button>
-            <label className="relative inline-block h-20 w-36">
+            <label
+              className="relative inline-block h-20 w-36"
+              htmlFor={`toggle-${data.id}`}
+            >
               <input
                 type="checkbox"
+                name={`toggle-${data.id}`}
+                id={`toggle-${data.id}`}
                 className="peer h-0 w-0 opacity-0"
                 defaultChecked={data.isActive}
+                aria-label={`Activate/Desactivate ${data.name} extension`}
               />
               <span className="absolute h-full w-full cursor-pointer rounded-3xl bg-neutral-300 transition-colors duration-300 peer-checked:bg-red-400 before:absolute before:bottom-2 before:left-2 before:h-16 before:w-16 before:rounded-full before:bg-white before:transition-transform before:duration-300 before:content-[''] peer-checked:before:translate-x-16"></span>
             </label>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -36,7 +36,7 @@ const Header = () => {
   return (
     <div className="m-auto flex max-w-[1156px] items-center justify-between gap-12 rounded-3xl border-2 border-neutral-200 bg-white p-12 shadow-[1px_1px_15px_0px_hsl(217,61%,90%)] dark:border-none dark:bg-neutral-800 dark:shadow-none">
       <div className="flex items-center gap-12">
-        <img src={logoSVG} alt="Logo" className="h-40 w-auto" />
+        <img src={logoSVG} alt="Logo Extensions Manager" className="h-40 w-auto" />
         <h2 className="text-2xl">Extensions</h2>
       </div>
       <button
@@ -45,9 +45,9 @@ const Header = () => {
         type="button"
       >
         {dark ? (
-          <img src={sun} alt="Sun image" className="h-24 w-auto" />
+          <img src={sun} alt="Sun illustration" className="h-24 w-auto" />
         ) : (
-          <img src={moon} alt="Moon image" className="h-24 w-auto" />
+          <img src={moon} alt="Moon illustration" className="h-24 w-auto" />
         )}
       </button>
     </div>
